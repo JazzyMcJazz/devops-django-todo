efbad81b795fdbbdd6c724d90fedea9605acd287
Lars Riel <larsriel@gmail.com>
2023-09-01 15:59:43 +0200

    made a todo app

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from todo import views
+
+app_name = 'todo'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('todos/', views.todos, name='todos'),
+    path('todos/<int:pk>/', views.todos, name='todos_pk'),
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, get_object_or_404
+from django.http import HttpResponseRedirect
+from todo.models import Todo
+from todo.forms import TodoForm
 
-# Create your views here.
+
+def index(request):
+    return HttpResponseRedirect(reverse('todo:todos'))
+
+    # todos = Todo.objects.all()
+    # todo_form = TodoForm()
+    # context = { 'todos': todos, 'todo_form': todo_form }
+    # return render(request, 'todo/index.html', context)
+
+def todos(request, pk=None):
+    todo_form = TodoForm()
+
+    if request.method == 'POST':
+        todo_form = TodoForm(request.POST)
+        if todo_form.is_valid():
+            todo_form.save()
+            todo_form = None
+
+    if request.method == 'DELETE':
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.delete()
+
+    if not todo_form:
+        todo_form = TodoForm()
+
+    context = { 'todos': Todo.objects.all(), 'todo_form': todo_form }
+    return render(request, 'todo/index.html', context)
